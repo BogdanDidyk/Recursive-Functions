f000ae8503fdbd627581615b1fd74790f7a27bf7
Bogdan Didyk <bogdandidyk26@gmail.com>
2023-06-04 22:58:25 +0300

    Added two clasic recursive algorithms: getFactorial() function and getFibonacci() function

---
+++ script.js
@@ -0,0 +1,10 @@
+function getFactorial(n) {
+    return (n == 0) ? 1 : n * getFactorial(n - 1);
+}
+
+function getFibonacci(n) {
+    return (n == 1 || n == 2) ? 1 : getFibonacci(n - 1) + getFibonacci(n - 2);
+}
+
+console.log(getFactorial(6));
+console.log(getFibonacci(6));
