c777afbe18b92229f575335fe2186643d47fa5d7
Bogdan Didyk <bogdandidyk26@gmail.com>
2023-06-05 22:33:50 +0300

    Added getDoubleFactorial() recursive function

--- script.js
+++ script.js
@@ -6,5 +6,10 @@ function getFibonacci(n) {
     return (n == 1 || n == 2) ? 1 : getFibonacci(n - 1) + getFibonacci(n - 2);
 }
 
+function getDoubleFactorial(n) {
+    return (n == 0 || n == 1) ? 1 : n * getDoubleFactorial(n - 2);
+}
+
 console.log(getFactorial(6));
 console.log(getFibonacci(6));
+console.log(getDoubleFactorial(6));
