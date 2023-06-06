f58dafece3124f251267c67dfa652fb4a3653114
Bogdan Didyk <bogdandidyk26@gmail.com>
2023-06-06 22:48:20 +0300

    Added getReversedString() recursive function

--- script.js
+++ script.js
@@ -14,7 +14,12 @@ function getSumOfDigitsInIntegerNumber(n) {
     return (n < 10) ? n : n % 10 + getSumOfDigitsInIntegerNumber(Math.floor(n / 10));
 }
 
+function getReversedString(str) {
+    return (str == "") ? "" : getReversedString(str.substring(1)) + str.charAt(0);
+}
+
 console.log(getFactorial(6));
 console.log(getFibonacci(6));
 console.log(getDoubleFactorial(6));
 console.log(getSumOfDigitsInIntegerNumber(5746));
+console.log(getReversedString("h3110 w0r1d"));
